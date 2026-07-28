495d92042770ff6684257c05a6bbb92f53b27062
2ktaly <frane8112@gmail.com>
2026-07-28 13:54:06 +0200

    Initial Commit

---
+++ ContactSwapTests/ContactSwapTests.swift
@@ -0,0 +1,18 @@
+//
+//  ContactSwapTests.swift
+//  ContactSwapTests
+//
+//  Created by Enrico Frank on 28.07.26.
+//
+
+import Testing
+
+struct ContactSwapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
